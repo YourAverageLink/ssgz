2fd8bf56eb9029f463efa9328346439577c0bb07
Xander DiGiovanni <37936141+YourAverageLink@users.noreply.github.com>
2025-06-26 21:28:17 -0500

    Dioxus UI (hopefully the github actions work)

--- src/patcher.rs
+++ src/patcher.rs
@@ -2,8 +2,6 @@ use std::fs;
 use std::fs::{OpenOptions, create_dir_all};
 use std::io::Write;
 
-use anyhow::Error;
-
 use crate::dol::Dol;
 use crate::iso_tools::GameVersion;
 use crate::patch_loader::{PatchData, get_patch_data};
@@ -11,14 +9,14 @@ use crate::paths::{
     custom_rel_path, extract_practice_saves_path, modified_dol_path, original_dol_path,
 };
 
-pub fn do_gz_patches(version: GameVersion) -> Result<(), Error> {
+pub fn do_gz_patches(version: GameVersion) -> anyhow::Result<()> {
     // Load necessary patch data for this version
     let patch_data = get_patch_data(version).unwrap();
     let og_dol: Vec<u8> = fs::read(&original_dol_path(version))?;
     let mut dol = Dol::new(og_dol);
 
     println!("Patching main.dol...");
-    patch_dol(&mut dol, &patch_data);
+    patch_dol(&mut dol, &patch_data)?;
     dol.save_changes();
 
     let mut dest_dol = OpenOptions::new()
@@ -39,7 +37,7 @@ pub fn do_gz_patches(version: GameVersion) -> Result<(), Error> {
     Ok(())
 }
 
-fn copy_practice_saves(patch_data: &PatchData) -> Result<(), Error> {
+fn copy_practice_saves(patch_data: &PatchData) -> anyhow::Result<()> {
     let target_path = extract_practice_saves_path(patch_data.version);
     // Only want to copy wiiking2.sav, skip.dat, and banner.bin
     let file_types = ["wiiking2.sav", "skip.dat", "banner.bin"];
@@ -69,7 +67,7 @@ fn copy_practice_saves(patch_data: &PatchData) -> Result<(), Error> {
     Ok(())
 }
 
-fn copy_custom_rel(patch_data: &PatchData) -> Result<usize, Error> {
+fn copy_custom_rel(patch_data: &PatchData) -> anyhow::Result<usize> {
     let mut dest_file = OpenOptions::new()
         .truncate(true)
         .read(true)
@@ -132,20 +130,22 @@ fn split_pointer_hi_lo(pointer: u32) -> (u32, u32) {
     (high_halfword, low_halfword)
 }
 
-fn patch_dol(dol: &mut Dol, patch_data: &PatchData) {
+fn patch_dol(dol: &mut Dol, patch_data: &PatchData) -> anyhow::Result<()> {
     let free_space_start = get_free_space_address(patch_data.version)
         .expect("Supported version must have free space addr defined to patch.");
 
     for (org_address, patch_bytes) in patch_data.patch_diffs.iter() {
         if *org_address >= free_space_start {
-            add_free_space_section(dol, patch_bytes, patch_data.version);
+            add_free_space_section(dol, patch_bytes, patch_data.version)?;
         } else {
-            dol.write_data_bytes(*org_address, patch_bytes);
+            dol.write_data_bytes(*org_address, patch_bytes)?;
         }
     }
+
+    Ok(())
 }
 
-fn add_free_space_section(dol: &mut Dol, bytes: &[u8], version: GameVersion) {
+fn add_free_space_section(dol: &mut Dol, bytes: &[u8], version: GameVersion) -> anyhow::Result<()> {
     // Need to make a copy and modify it to satisfy the borrow checker
     let mut dol_section = dol.sections[2];
     let patch_length = bytes.len() as u32;
@@ -160,7 +160,7 @@ fn add_free_space_section(dol: &mut Dol, bytes: &[u8], version: GameVersion) {
 
     dol.sections[2] = dol_section;
     // Write custom code to the end of the dol
-    dol.write_data_bytes(dol_section.address, bytes);
+    dol.write_data_bytes(dol_section.address, bytes)?;
 
     let padded_patch_length = (patch_length + 3) & 0xFFFFFFFC;
     let new_start_ptr = dol.sections[2].address + padded_patch_length;
@@ -175,22 +175,24 @@ fn add_free_space_section(dol: &mut Dol, bytes: &[u8], version: GameVersion) {
     dol.write_data_u32(
         thread_stack_update_end_locations[0],
         0x3CA00000 | high_halfword,
-    );
+    )?;
     dol.write_data_u32(
         thread_stack_update_end_locations[1],
         0x38A50000 | low_halfword,
-    );
+    )?;
 
     // More hardcoded pointers that come later
     let new_end_ptr = new_start_ptr + 0x10000;
     let (high_halfword, low_halfword) = split_pointer_hi_lo(new_end_ptr);
     for loc_row in &thread_stack_update_start_locations {
-        dol.write_data_u32(loc_row[0], 0x3C600000 | high_halfword);
-        dol.write_data_u32(loc_row[1], 0x38630000 | low_halfword);
+        dol.write_data_u32(loc_row[0], 0x3C600000 | high_halfword)?;
+        dol.write_data_u32(loc_row[1], 0x38630000 | low_halfword)?;
     }
 
     let high_halfword = (new_end_ptr & 0xFFFF0000) >> 16;
     let low_halfword = new_end_ptr & 0xFFFF;
-    dol.write_data_u32(0x80004284, 0x3C200000 | high_halfword);
-    dol.write_data_u32(0x80004288, 0x60210000 | low_halfword);
+    dol.write_data_u32(0x80004284, 0x3C200000 | high_halfword)?;
+    dol.write_data_u32(0x80004288, 0x60210000 | low_halfword)?;
+
+    Ok(())
 }
